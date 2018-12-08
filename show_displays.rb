f0cd782469990c50177c96a7765c4e9dbc34adc4
matiyan <wonderland.bana@gmail.com>
2018-12-08 06:20:38 +0000

    各画面の表示関数のひな型を作成。

---
+++ show_displays.rb
@@ -0,0 +1,18 @@
+#encoding: utf-8
+
+# **** To show displays functions ****
+
+def title()
+    Window.draw_font(Window.width / 2, Window.height / 2, "Title", Font.default, color: C_BLACK
+
+end
+
+def game_main()
+    Window.draw_font(Window.width / 2, Window.height / 2, "game main", Font.default, color: C_BLACK
+
+end
+
+def result()
+    Window.draw_font(Window.width / 2, Window.height / 2, "result", Font.default, color: C_BLACK
+
+end
