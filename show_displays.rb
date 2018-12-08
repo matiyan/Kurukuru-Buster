6d335633fa40c7c981d9cee93363c269461546cf
matiyan <wonderland.bana@gmail.com>
2018-12-08 07:44:18 +0000

    メソッド名を間違えていたのを修正。

--- show_displays.rb
+++ show_displays.rb
@@ -1,21 +1,19 @@
-#encoding: utf-8
+
 
 # **** To show displays functions ****
 
 def title()
-    Window.draw_font(Window.width / 2, Window.height / 2, "Title", Font.default, color: C_BLACK
-        until Window.push_key? do end
+    Window.draw_font(Window.width / 2, Window.height / 2, "Title", Font.default, color: C_BLACK)
 
 end
 
 def game_main()
-    Window.draw_font(Window.width / 2, Window.height / 2, "game main", Font.default, color: C_BLACK
-    until Window.push_key? do end
+
+    Window.draw_font(Window.width / 2, Window.height / 2, "game main", Font.default, color: C_BLACK)
 
 end
 
 def result()
-    Window.draw_font(Window.width / 2, Window.height / 2, "result", Font.default, color: C_BLACK
-    until Window.push_key? do end
-
+    Window.draw_font(Window.width / 2, Window.height / 2, "result", Font.default, color: C_BLACK)
+ 
 end
