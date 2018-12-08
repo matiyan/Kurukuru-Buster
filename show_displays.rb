0359627a467ead845eb9a46ef95031afb5950365
matiyan <wonderland.bana@gmail.com>
2018-12-08 07:02:08 +0000

    ひとまず処理が終わらないようにループ化した。

--- show_displays.rb
+++ show_displays.rb
@@ -4,15 +4,18 @@
 
 def title()
     Window.draw_font(Window.width / 2, Window.height / 2, "Title", Font.default, color: C_BLACK
+        until Window.push_key? do end
 
 end
 
 def game_main()
     Window.draw_font(Window.width / 2, Window.height / 2, "game main", Font.default, color: C_BLACK
+    until Window.push_key? do end
 
 end
 
 def result()
     Window.draw_font(Window.width / 2, Window.height / 2, "result", Font.default, color: C_BLACK
+    until Window.push_key? do end
 
 end
