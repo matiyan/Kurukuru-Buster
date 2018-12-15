12bd2164dcb84a5643a1ff39d36cb7d46623d586
Aozasa <oor.as1999@gmail.com>
2018-12-15 00:36:14 +0000

    mod

--- player.rb
+++ player.rb
@@ -9,7 +9,7 @@ class Player < Sprite
         self.center_x = center_x
         self.center_y = center_y
     end
-    x
+    
     def change_dir()
         self.angle += Input.x
         @dir = (self.angle / 180) * PI
