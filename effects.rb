e950b24dbedec6d6d86c0bbc1e722b035cf7002e
matiyan <wonderland.bana@gmail.com>
2018-12-22 10:54:34 +0900

    Effectを管理するEffectsクラスを追加

---
+++ effects.rb
@@ -0,0 +1,16 @@
+class Effects
+	def initialize imgs
+		@items = []
+		@imgs = imgs
+	end
+
+	def add x, y
+		@items << Effect.new x, y, @imgs
+	end
+	
+	def update
+		@items.each{|i| i.draw}
+
+		@items = @items.reject{|i| i.finish?}
+	end
+end
